8373629d1f3bb3c86677d0f81f13f91a647b7c47
Kana28 <kaichum24@gmail.com>
2021-03-17 08:52:08 +0000

    update

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -6,8 +6,16 @@ class Customers::ProductsController < ApplicationController
   end
 
   def index
+    @products = Product.where(is_sale: true).page(params[:page]).per(8)
   end
 
   def show
   end
+
+
+
+	private
+	def product_params
+		parmas.require(:product).permit(:name, :introduction, :nontaxed_price, :image_id, :is_active)
+	end
 end
