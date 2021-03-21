f7858dd9f8b381ad8ffb213d7e65f0c95aa38de3
masanor57 <miyajim2020@gmail.com>
2021-03-21 01:12:12 +0000

    update1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-<<<<<<< HEAD
   
   
   protected
@@ -9,10 +8,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postcode, :phone_number])
   devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postcode, :phone_number])
-=======
+  end
 
   def set_cart_product
     @cart_product = CartProduct.find(params[:id])
->>>>>>> 0c4bff0a399e440a833a9ee25a15a4f2c1a8ef56
   end
 end
