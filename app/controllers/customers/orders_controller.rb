b83b52c428724686102286f4855ac0e505c9f55d
Kana28 <kaichum24@gmail.com>
2021-03-21 07:55:55 +0000

    update6

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -5,6 +5,11 @@ class Customers::OrdersController < ApplicationController
   end
 
   def log
+    @cart_products = current_customer.cart_products.all
+	  @order = Order.new(
+      customer: current_customer,
+      payment_method: params[:order][:payment_method]
+    )
 
   end
 
@@ -13,8 +18,14 @@ class Customers::OrdersController < ApplicationController
     @order = current_customer.orders.new(order_params)
     @order.save
     redirect_to customers_orders_thanx_path
+    
+   # もし情報入力でnew_addressの場合Addressに保存
+ 
 
    # カート商品の情報を注文商品に移動(カート機能で保存された商品を一括決済)
+
+
+
 
 
    # 注文完了後、カート商品を空にする
