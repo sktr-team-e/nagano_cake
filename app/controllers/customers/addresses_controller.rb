fb24c5b16631319179f119359244b8d87d833ab0
syokupanda <borun.books@gmail.com>
2021-03-22 06:37:22 +0000

    save

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,8 +1,14 @@
 class Customers::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+    @customer = current_customer
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to customers_addresses_path
   end
 
   def destroy
@@ -13,4 +19,10 @@ class Customers::AddressesController < ApplicationController
 
   def update
   end
+
+private
+  def address_params
+    params.require(:address).permit(:name, :postcode, :address, :customer_id)
+  end
+
 end
