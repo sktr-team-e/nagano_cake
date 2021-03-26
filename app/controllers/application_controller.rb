95323e32f2a7688acebda07cf9ed99e25a2b0ed5
masanor57 <miyajim2020@gmail.com>
2021-03-26 04:48:28 +0000

    update2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,34 @@ class ApplicationController < ActionController::Base
   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postcode, :phone_number])
   devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postcode, :phone_number])
   end
+  
+  def after_sign_up_path_for(resource)
+       customers_path(resource)
+  end
+  
+  def after_inactive_sign_up_path_for(resource)
+    　customers_path(resource)
+  end
+  
+  
+  def after_sign_in_path_for(resource)
+     if resource.instance_of?(Admin)
+        admins_orders_path
+     elsif resource.instance_of?(Customer)
+        root_path
+     else
+        root_path
+     end
+  end
+  
 
+ def after_sign_out_path_for(resource)
+     if resource == :admin
+        new_admin_session_path
+     elsif resource == :customer
+        root_path
+     else
+        root_path
+     end
+ end
 end
